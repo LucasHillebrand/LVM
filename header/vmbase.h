651b9a2fd8e4690379b070c2d27eba313b0f17b0
LucasHillebrand <lucashillebrand05@gmail.com>
2023-05-14 16:09:44 +0200

    reorginizing the instructions

--- header/vmbase.h
+++ header/vmbase.h
@@ -33,29 +33,28 @@ typedef struct SOC {
 
 #define CMD_STORE 3 // stores curr val in mem
 #define CMD_LOAD 4 // loads value from mem
+#define CMD_SMP 5 // set mem ptr
 
-#define CMD_JUMP 5 // jumps to an specific instruction
-#define CMD_JIZ 6 // jump if zero
-#define CMD_JNZ 7 // jump if not zero
-
-#define CMD_SMP 8 // set mem ptr
+#define CMD_JUMP 6 // jumps to an specific instruction
+#define CMD_JIZ 7 // jump if zero
+#define CMD_JNZ 8 // jump if not zero
 
 #define CMD_MOV 9 // move curr to register
 #define CMD_PSH 10 // push register to curr
 
 #define CMD_DB 11 // define byte
 #define CMD_PV 12 // define register
+#define CMD_PI 13 // push pc/index
 
-#define CMD_SPM 13 // set pinmode
-#define CMD_SPP 14 // set pin ptr
-#define CMD_RPV 15 // read pin value
-#define CMD_WPV 16 // write pin value
+#define CMD_SPM 14 // set pinmode
+#define CMD_SPP 15 // set pin ptr
+#define CMD_RPV 16 // read pin value
+#define CMD_WPV 17 // write pin value
 
-#define CMD_NTH 17 // new thread
-#define CMD_STH 18 // stop thread
-#define CMD_HLT 19 // halt machine
+#define CMD_NTH 18 // new thread
+#define CMD_STH 19 // stop thread
 
-#define CMD_PI 20 // push pc
+#define CMD_HLT 255 // halt machine
 
 SOC VM_ADD(SOC self, byte thread, byte args[3]);
 SOC VM_SUB(SOC self, byte thread, byte args[3]);
